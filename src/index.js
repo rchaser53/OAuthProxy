e80859224940befda96a87edc980b9a46d8caff1
rchaser53 <dusk41@gmail.com>
2016-07-14 23:52:05 +0900

    try to twitter post

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 window.onload = function(){
 	document.querySelector("#submitBtn").addEventListener("click",(e)=>{
+		// fetch("http://localhost:3000/proxy",{
+		// 	method:"GET",
+		// 	headers:{
+		// 		"twitterQuery":'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=rchaser53&count=1'
+		// 	}
+
 		fetch("http://localhost:3000/proxy",{
-			method:"GET",
+			method:"POST",
 			headers:{
-				"twitterQuery":'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=rchaser53&count=1'
-			}
+				"twitterQuery":'https://api.twitter.com/1.1/statuses/update.json?status='
+			},
+			body:JSON.stringify({
+    			status:"test"
+    		})
 		}).then((response)=>{
 			 return response.json();
 		}).then((ret)=>{
--- tasks/proxy.js
+++ tasks/proxy.js
@@ -25,25 +25,39 @@ module.exports = (options)=>{
       consumerSecret,
       version,
       authorize_callback,
-      signatureMethod
+      signatureMethod,
+      32,
+      {
+        // "Accept" : "*/*",
+        // "Connection" : "close",
+        // "User-Agent" : "Node authentication",
+        // "status":JSON.stringify({
+        //     status:"test"
+        // })
+      }
     );
 
     const query = req.header("twitterQuery");
 
     if(req.header("twitterQuery") != null){
-      oauth.get(
-          query,
+      oauth.post(
+          query + "test",
           userToken,
           useSecretToken,
+          JSON.stringify({
+            status:"test"
+          }),
+          null,
           (err, data, response)=>{
             if (err){
               console.error(err);
-              return;
+              console.log(response.headers);
             }
             res.header("Access-Control-Allow-Origin","http://localhost:8000");
             res.header("Access-Control-Allow-Headers","twitterQuery");
             res.send(data);
       });
+
     } else{
       res.header("Access-Control-Allow-Origin","http://localhost:8000");
       res.header("Access-Control-Allow-Headers","twitterQuery");
