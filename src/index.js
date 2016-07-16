9542f9c96aacb0ce564fd60e6c0245cf5efb8996
rchaser53 <dusk41@gmail.com>
2016-07-16 15:07:39 +0900

    make app.get and app.post

--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
 window.onload = function(){
-	document.querySelector("#submitBtn").addEventListener("click",(e)=>{
-		// fetch("http://localhost:3000/proxy",{
-		// 	method:"GET",
-		// 	headers:{
-		// 		"twitterQuery":'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=rchaser53&count=1'
-		// 	}
 
-		fetch("http://localhost:3000/proxy",{
-			method:"POST",
-			headers:{
-				"twitterQuery":'https://api.twitter.com/1.1/statuses/update.json?status='
-			},
-			body:JSON.stringify({
-    			status:"test"
-    		})
-		}).then((response)=>{
-			 return response.json();
-		}).then((ret)=>{
-			console.log(ret);
-		}).catch((err)=>{
-			console.log(err);
-		})
+
+	document.querySelector("#submitBtn").addEventListener("click",(e)=>{
+		getTweet("GET","screen_name=rchaser53&count=5")
+			.then((response)=>{
+				 return response.json();
+			}).then((ret)=>{
+				console.log(ret);
+			}).catch((err)=>{
+				console.log(err);
+			});
+
+
+		// fetch("http://localhost:3000/proxy",{
+		// 	method:"POST"
+			// headers:{
+			// 	"twitterQuery":'https://api.twitter.com/1.1/statuses/update.json?status='
+			// }
+			// body:JSON.stringify({
+			// 	status:"test"
+			// })
+		// }).then((response)=>{
+		// 	 return response.json();
+		// }).then((ret)=>{
+		// 	console.log(ret);
+		// }).catch((err)=>{
+		// 	console.log(err);
+		// })
 	})
+}
+
+const getTweet = (method,query) => {
+	return fetch("http://localhost:3000/proxy",{
+		method:"GET",
+		headers:{
+			twitterQuery:query
+		}
+	});
 }
--- tasks/server.js
+++ tasks/server.js
@@ -29,21 +29,50 @@ module.exports = ()=>{
     signatureMethod
   );
 
-  app.all('/proxy/?*',proxy(oauth));
+  app.get('/proxy',(req,res,next)=>{
+      const query = req.header("twitterQuery");
 
-  app.get('/proxy',(req,res)=>{
-      console.log("get");
+      oauth.get(
+          'https://api.twitter.com/1.1/statuses/user_timeline.json?' + query,
+          userToken,
+          useSecretToken,
+          resProcess
+      );
   })
 
-  app.post('/proxy',(req,res)=>{
-      console.log("post");
-      res.header("Access-Control-Allow-Origin","http://localhost:8000");
-      res.header("Access-Control-Allow-Headers","twitterQuery");
-      res.send();
+  app.post('/proxy',(req,res,next)=>{
+      const query = req.header("twitterQuery");
+
+      oauth.post(
+          'https://api.twitter.com/1.1/statuses/update.json?status=' + query,
+          userToken,
+          useSecretToken,
+          JSON.stringify({
+              status:"test"
+          }),
+          null,
+          resProcess
+      );
   })
 
-  http.createServer(app).listen(3000, function (e) {
+  app.options("/*",(req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","http://localhost:8000");
+    res.header("Access-Control-Allow-Headers","twitterQuery");
+    res.end();
+    return
+  })
+
+  http.createServer(app).listen(3000, (e)=>{
       console.log("express server listening on port 3000")
   });
 
+}
+
+const resProcess = (err,data,response) =>{
+  if (err){
+    console.error(err);
+  }
+  res.header("Access-Control-Allow-Origin","http://localhost:8000");
+  res.header("Access-Control-Allow-Headers","twitterQuery");
+  res.send(data);
 }
