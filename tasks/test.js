12f2a0444fd056f94d97e3dd71d46523a5f48c08
rchaser53 <dusk41@gmail.com>
2016-07-19 23:11:29 +0900

    delete test.js

--- tasks/test.js
+++
@@ -1,8 +0,0 @@
-const vfs = require("vinyl-fs");
-const zip = require("gulp-zip");
-
-const componentName = "test"
-
-vfs.src('./dest/*')
-  .pipe(zip(componentName + '.resource'))
-  .pipe(vfs.dest('./pkg/staticresources'));
